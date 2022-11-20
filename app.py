fac14331ad4334bdf3257d0aa3915111c56b1e7c
Jebin Peter <jebinpeter.jp@gmail.com>
2022-11-20 22:19:52 +0530

    make it better

--- app.py
+++ app.py
@@ -1,53 +1,71 @@
 from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
 import pickle
 import numpy as np
 
 
 app = Flask(__name__)
-
+api = Api(app)
 with open('model_jebin','rb') as f:
 
     model = pickle.load(f)
 
-@app.route('/')
-def home():
-    return "Hello world"
+# @app.route('/')
+# def home():
+#     return "Hello world"
 
 
-@app.route('/predict',methods = ['GET'])
+# @app.route('/predict',methods = ['GET','POST'])
 
-def predict():
-    happy = request.form.get('happy')
-    sad = request.form.get('sad')
-    apetite = request.form.get('apetite')
-    stress = request.form.get('stress')
+# def predict():
+#     print(request)
+#     happy = request.body.get('happy')
+#     sad = request.body.get('sad')
+#     apetite = request.body.get('apetite')
+#     stress = request.body.get('stress')
    
-    ratings = [happy,sad,apetite,stress]
-    b = np.array(ratings, dtype=float)
+#     ratings = [happy,sad,apetite,stress]
+#     b = np.array(ratings, dtype=float)
     
-    input_data = (b)
-    input_data_as_numpy_array = np.asarray(input_data)
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+#     input_data = (b)
+#     input_data_as_numpy_array = np.asarray(input_data)
+#     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-    result = model.predict(input_data_reshaped)[0]
+#     result = model.predict(input_data_reshaped)[0]
 
-    if result == 1:
-        return jsonify({'mood': str('good')})
-    else:
-        return jsonify({'mood': str('bad')})
+#     if result == 1:
+#         return jsonify({'mood': str('good')})
+#     else:
+#         return jsonify({'mood': str('bad')})
 
 
-# class getEmotion(Resource):
+class getEmotion(Resource):
   
     
-#     def post(self):
+    # def post(self):
           
-#         data = request.get_json()     # status code
-#         return jsonify({'data': data}), 201
-  
+    #     data = request.get_json()     # status code
+    #     return jsonify({'data': data}), 201
+    def post(self):
+        print(request.get_json())
+        data = request.get_json()
+    
+        ratings = [data['happy'],data['sad'],data['apetite'],data['stress']]
+        b = np.array(ratings, dtype=float)
+        
+        input_data = (b)
+        input_data_as_numpy_array = np.asarray(input_data)
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+        result = model.predict(input_data_reshaped)[0]
+
+        if result == 1:
+            return jsonify({'mood': str('good')})
+        else:
+            return jsonify({'mood': str('bad')})
   
 # # adding the defined resources along with their corresponding urls
-# api.add_resource(getEmotion, '/')
+api.add_resource(getEmotion, '/')
   
   
 # driver function
